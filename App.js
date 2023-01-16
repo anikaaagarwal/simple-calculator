746441322b96f9ca80aff65ccb704e030c80fab3
anikaaagarwal <88231460+anikaaagarwal@users.noreply.github.com>
2023-01-16 14:08:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,162 @@
+import { useReducer } from 'react'
+import Digitbutton from './Digitbutton'
+import Operationbutton from './Operationbutton'
+import './styles.css'
+
+export const ACTIONS = {
+    ADD_DIGIT: 'add-digit',
+    CLEAR: 'clear',
+    DELETE_DIGIT: 'delete-digit',
+    EVALUATE: 'evaluate',
+}
+
+function reducer(state, { type, payload }) {
+    // eslint-disable-next-line default-case
+    switch (type) {
+        case ACTIONS.ADD_DIGIT:
+            if (state.ADD_DIGIT) {
+                return {
+                    ...state,
+                    currentOperand: payload.digit,
+                    overwrite: false,
+                }
+            }
+            if (payload.digit === "0" && state.currentOperand === "0") {
+                return state
+            }
+            if (payload.digit === "." && state.currentOperand.includes(".")) {
+                return state
+            }
+
+            return {
+                ...state,
+                currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+            }
+        case ACTIONS.CHOOSE_OPERATION:
+            if (state.currentOperand == null && state.previousOperand == null) {
+                return state
+            }
+        
+            if (state.currentOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,     
+                }
+            }
+
+            if (state.previousOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,
+                    previousOperand: state.currentOperand,
+                    currentOperand: null,
+                }
+            }
+
+            return {
+                ...state,
+                previousOperand: evaluate(state),
+                operation: payload.operation,
+                currentOperand: null
+            }
+        case ACTIONS.CLEAR:
+            return {}
+        case ACTIONS.DELETE_DIGIT:
+            if (state.overwrite) { 
+                return {
+                    ...state,
+                    overwrite: false,
+                    currentOperand: null,
+                }
+            }
+            if (state.currentOperand == null ) return state
+            if (state.currentOperand.length === 1) 
+                return {
+                    ...state,
+                    currentOperand: null,
+                }
+                
+                return {
+                    ...state, 
+                    currentOperand: state.currentOperand.slice(0,-1)
+                }
+        case ACTIONS.EVALUATE:
+            if (state.operation == null || state.currentOperand == null || state.previousOperand == null) {
+               return state 
+            }
+
+            return {
+                ...state,
+                overwrite: true, 
+                operation: null,
+                currentOperand: evaluate(state) 
+            }
+    }
+}
+
+function evaluate({currentOperand, previousOperand, operation}){
+    const prev = parseFloat(previousOperand)
+    const current = parseFloat(currentOperand)
+    if (isNaN(prev) || isNaN(current)) return ""
+    let computation = ""
+    // eslint-disable-next-line default-case
+    switch (operation) {
+        case "+":
+            computation = prev + current
+            break
+        case "-":
+            computation = prev - current 
+            break
+        case "*":
+            computation = prev * current
+            break
+        case "/":
+            computation = prev / current
+            break 
+    }
+
+    return computation.toString()
+}
+
+const integer_formatter = new Intl.NumberFormat("en-us", {
+    maximumFractionDigits: 0,
+})
+function formatOperand(operand){
+    if (operand == null) return 
+    const [integer, decimal] = operand.split('.')
+    if (decimal == null) return integer_formatter.format(integer)
+    return `${integer_formatter.format(integer)}.${decimal}`
+}
+
+function App() {
+    const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+    
+    return ( 
+        <div className = "calculator-grid">
+            <div className = "output">
+                <div className = "previous-operand">{formatOperand(previousOperand)} {operation}</div>  
+                <div className = "current-operand">{formatOperand(currentOperand)}</div>  
+            </div>  
+            <button className = "span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})}>AC</button> 
+            <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT})}>DEL</button>
+            <Operationbutton operation="/" dispatch={dispatch}/> 
+            <Digitbutton digit="1" dispatch={dispatch}/> 
+            <Digitbutton digit="2" dispatch={dispatch}/> 
+            <Digitbutton digit="3" dispatch={dispatch}/>  
+            <Operationbutton operation="*" dispatch={dispatch}/>
+            <Digitbutton digit="4" dispatch={dispatch}/> 
+            <Digitbutton digit="5" dispatch={dispatch}/> 
+            <Digitbutton digit="6" dispatch={dispatch}/> 
+            <Operationbutton operation="+" dispatch={dispatch}/>
+            <Digitbutton digit="7" dispatch={dispatch}/> 
+            <Digitbutton digit="8" dispatch={dispatch}/> 
+            <Digitbutton digit="9" dispatch={dispatch}/> 
+            <Operationbutton operation="-" dispatch={dispatch}/>
+            <Digitbutton digit="." dispatch={dispatch}/> 
+            <Digitbutton digit="0" dispatch={dispatch}/>  
+            <button className = "span-two" onClick={() => dispatch({type: ACTIONS.EVALUATE})}>=</button>  
+        </div>
+    )
+}
+
+export default App
